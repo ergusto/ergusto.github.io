d8a8db5d257fdd4b3f10b8988c431ac01abefaf2
Fergus Ruston <fergus.ruston@gmail.com>
2016-10-16 01:46:17 +0200

    introduction animation timing

--- app/components/introduction/index.jsx
+++ app/components/introduction/index.jsx
@@ -69,7 +69,7 @@ export default class IntroductionComponent extends React.Component {
 
 										}, 300);
 									}, 700);
-								}, 700);
+								}, 600);
 							});
 						});
 					}, 300);
