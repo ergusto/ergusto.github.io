ec06022e29c3b7ddeb18fe2957c96f4b8860436b
Fergus Ruston <fergus.ruston@gmail.com>
2019-01-06 22:38:59 +0000

    removed cname and added css utility classes

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -7,8 +7,8 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div className="full-height background-color-dark-blue justify-centre comfortaa color-white">
-				<div className="width-100 align-center">
-					<h1>ergusto</h1>
+				<div className="align-center">
+					<h1 className="margin-bottom-medium">ergusto</h1>
 					<p>New site coming soon</p>
 				</div>
 			</div>
