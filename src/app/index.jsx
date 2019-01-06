368c0f941ded8ad0bb398d2677a123dd99cbf7fe
Fergus Ruston <fergus.ruston@gmail.com>
2019-01-06 21:42:16 +0000

    placeholder

---
+++ src/app/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../css/site.scss';
+
+export default class App extends React.Component {
+
+	render() {
+		return (
+			<div className="full-height background-color-dark-blue justify-centre comfortaa color-white">
+				<div className="width-100 align-center">
+					<h1>ergusto</h1>
+					<p>New site coming soon</p>
+				</div>
+			</div>
+		);
+	}
+
+}
