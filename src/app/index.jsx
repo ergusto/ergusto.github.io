8dbccace438b0024faa70305a2c92c3b40971232
Fergus Ruston <fergus.ruston@gmail.com>
2019-01-07 00:23:27 +0000

    updated cname

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,7 @@
 import 'typeface-comfortaa';
 import React from 'react';
 import viewportBuggyfill from 'viewport-units-buggyfill';
+import Introduction from './modules/introduction/index.jsx';
 
 import '../css/site.scss';
 
@@ -10,12 +11,7 @@ export default class App extends React.Component {
 
 	render() {
 		return (
-			<div className="full-height background-color-dark-blue justify-centre comfortaa color-white">
-				<div className="align-center">
-					<h1 className="margin-bottom-medium">ergusto</h1>
-					<p>New project coming soon</p>
-				</div>
-			</div>
+			<Introduction />
 		);
 	}
 
---
+++ src/app/modules/introduction/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+	render() {
+		return (
+			<div className="full-height background-color-dark-blue justify-centre comfortaa color-white">
+				<div className="align-center">
+					<h1 className="margin-bottom-medium">ergusto</h1>
+					<p>New project coming soon</p>
+				</div>
+			</div>
+		);
+	}
+
+}
